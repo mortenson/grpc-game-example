eba036c10053a103f19b84480687f2e25c278372
Sam Mortenson <samuel@mortenson.coffee>
2020-04-13 21:28:59 -0700

    Prevented lasers from going out of bounds.

--- pkg/backend/laser.go
+++ pkg/backend/laser.go
@@ -19,7 +19,7 @@ type Laser struct {
 
 func (laser *Laser) Position() Coordinate {
 	difference := time.Now().Sub(laser.StartTime)
-	moves := int(math.Floor(float64(difference.Milliseconds()) / float64(20)))
+	moves := int(math.Floor(float64(difference.Milliseconds()) / float64(21)))
 	position := laser.InitialPosition
 	switch laser.Direction {
 	case DirectionUp:
